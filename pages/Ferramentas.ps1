2f8de0ebeac41b9e80d210ea012f7984e9edc093
Daniel <danielghstm@gmail.com>
2026-01-23 03:57:00 +0000

    Refactor Ferramentas.ps1 layout and update positions

--- pages/Ferramentas.ps1
+++ pages/Ferramentas.ps1
@@ -8,20 +8,21 @@ function Render-Ferramentas {
     Add-Launcher -Text "Painel de`nControle" -Command "control" -X 170 -Y 70
     Add-Launcher -Text "Editor de`nRegistro" -Command "regedit" -X 320 -Y 70
     Add-Launcher -Text "CMD`n(Admin)" -Command "cmd" -X 470 -Y 70
-    Add-Launcher -Text "Problem Steps Recorder`nPSR" -Command "psr.exe" -X 620 -Y 70              
+    Add-Launcher -Text "Problem Steps Recorder (PSR)" -Command "psr.exe" -X 620 -Y 70              
     
     # Linha 2
-    Add-Launcher -Text "Limpeza de`nDisco" -Command "cleanmgr" -X 20 -Y 180
-    Add-Launcher -Text "Serviços" -Command "services.msc" -X 170 -Y 180
-    Add-Launcher -Text "Conexões" -Command "ncpa.cpl" -X 320 -Y 180
-    Add-Launcher -Text "Windows`nUpdate" -Command "ms-settings:windowsupdate" -X 470 -Y 180
-    Add-Launcher -Text "Event Viewer" -Command "eventvwr.msc" -X 620 -Y 180             
+    Add-Launcher -Text "Limpeza de`nDisco" -Command "cleanmgr" -X 20 -Y 150
+    Add-Launcher -Text "Serviços" -Command "services.msc" -X 170 -Y 150
+    Add-Launcher -Text "Conexões" -Command "ncpa.cpl" -X 320 -Y 150
+    Add-Launcher -Text "Windows`nUpdate" -Command "ms-settings:windowsupdate" -X 470 -Y 150
+    Add-Launcher -Text "Event Viewer" -Command "eventvwr.msc" -X 620 -Y 150             
 
     # Linha 3
-    Add-Launcher -Text "Configuração do Sistema" -Command "msconfig" -X 20 -Y 290   
-    Add-Launcher -Text "Gerenciador de Dispositivos" -Command "devmgmt.msc" -X 170 -Y 290
-    Add-Launcher -Text "Monitor de Recursos" -Command "resmon" -X 320 -Y 290
-    Add-Launcher -Text "Monitor de Desempenho" -Command "perfmon" -X 470 -Y 290 
-    Add-Launcher -Text "Diagnóstico de Memória" -Command "mdsched.exe" -X 620 -Y 290    
+    Add-Launcher -Text "Configuração do Sistema" -Command "msconfig" -X 20 -Y 230   
+    Add-Launcher -Text "Gerenciador de Dispositivos" -Command "devmgmt.msc" -X 170 -Y 230
+    Add-Launcher -Text "Monitor de Recursos" -Command "resmon" -X 320 -Y 230
+    Add-Launcher -Text "Monitor de Desempenho" -Command "perfmon" -X 470 -Y 230 
+    Add-Launcher -Text "Diagnóstico de Memória" -Command "mdsched.exe" -X 620 -Y 230    
 
 }
+
