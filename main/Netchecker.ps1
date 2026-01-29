0bfa2ec81061a6dbbb6955a025451987b621a1c9
Daniel <danielghstm@gmail.com>
2026-01-29 03:09:00 +0000

    Refactor Netchecker function in Netchecker.ps1
    Refactor Netchecker function to improve readability and compactness.

--- main/Netchecker.ps1
+++ main/Netchecker.ps1
@@ -3,15 +3,12 @@
 # Script de verificacao de dispositivos.
 
 function Netchecker {
-	
+		# Coleta e retorno das infos de rede.
 		$netc = Get-CimInstance Win32_NetworkAdapterConfiguration  -Filter "IPEnabled = True"
-	
-		[PSCustomObject]@{
-		IP = $netc.IPAddress[0]
-		DNS1 = $netc.DNSServerSearchOrder[0]
-		DNS2 = $netc.DNSServerSearchOrder[1]
+		[PSCustomObject]@{IP = $netc.IPAddress[0];DNS1 = $netc.DNSServerSearchOrder[0];DNS2 = $netc.DNSServerSearchOrder[1]}
 		}
 }
 
 $global:rnetc = Netchecker
+
 Write-Host "IP: $($rnetc.IP) / DNS1: $($rnetc.DNS1) / DNS2: $($rnetc.DNS2)" -ForegroundColor Cyan
