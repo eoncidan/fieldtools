c2a8ca0eff85faa9f204e3cdb3033d72ee154a3f
Daniel <danielghstm@gmail.com>
2026-02-05 22:18:19 -0300

    Modify ScrollPanel size and update comments
    Adjusted the size of the ScrollPanel and added a comment for app installation.

--- pages/Apps.ps1
+++ pages/Apps.ps1
@@ -6,13 +6,14 @@ function Render-Apps {
     # Painel com Scroll para a lista de apps
     $ScrollPanel = New-Object System.Windows.Forms.FlowLayoutPanel
     $ScrollPanel.Location = New-Object System.Drawing.Point(20, 70)
-    $ScrollPanel.Size = New-Object System.Drawing.Size(800, 450)
+    $ScrollPanel.Size = New-Object System.Drawing.Size(800, 430)
     $ScrollPanel.AutoScroll = $true
     $ScrollPanel.FlowDirection = "TopDown"
     $ScrollPanel.WrapContents = $false
     $script:ContentPanel.Controls.Add($ScrollPanel)
 
     # Lista de Apps para instalar
+   #Add-WingetApp -AppName "Nome do app" -WingetID "Caminho mstore" -Description "Descrição do app" -ParentPanel $ScrollPanel	
     Add-WingetApp -AppName "Notepad++" -WingetID "Notepad++.Notepad++" -Description "Editor de texto avançado" -ParentPanel $ScrollPanel
     Add-WingetApp -AppName "WinToys" -WingetID "9P8LTPGCBZXD" -Description "Otimização e personalização do Windows" -ParentPanel $ScrollPanel
     Add-WingetApp -AppName "HWMonitor" -WingetID "CPUID.HWMonitor" -Description "Monitoramento de hardware e temperaturas" -ParentPanel $ScrollPanel
@@ -21,3 +22,4 @@ function Render-Apps {
     Add-WingetApp -AppName "Lightshot" -WingetID "Skillbrains.Lightshot" -Description "Ferramenta de captura de tela rápida" -ParentPanel $ScrollPanel
     Add-WingetApp -AppName "LibreOffice" -WingetID "TheDocumentFoundation.LibreOffice" -Description "Suíte de escritório gratuita e open source" -ParentPanel $ScrollPanel
 }
+
